b709a1c17c80bc3b59fbfc3d39acf9581efb7fc4
davidmaciell <david.maciel94@gmail.com>
2023-01-23 23:43:46 -0300

    sistema conecta com banco e salva dados

--- cadastro/FormCadastroClient.cs
+++ cadastro/FormCadastroClient.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace TapecariaSystem.cadastro
 {
     public partial class FormCadastroClient : Form
     {
+        Conexao con = new Conexao();
+        string sql;
+        MySqlCommand cmd;
         public FormCadastroClient()
         {
             InitializeComponent();
@@ -19,7 +23,20 @@ namespace TapecariaSystem.cadastro
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            con.AbrirConexao();
+            sql = "INSERT INTO tb_cliente ( nome_cliente, endereco_cliente, cep_cliente, cpf_cliente, telefone_cliente, celular_cliente)VALUES( @nome, @endereco, @cep, @cpf, @telefone, @celular)";
 
+            cmd = new MySqlCommand(sql, con.con);
+
+            cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+            cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+            cmd.Parameters.AddWithValue("@cep", txtCep.Text);
+            cmd.Parameters.AddWithValue("@cpf", txtCpf.Text);
+            cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+            cmd.Parameters.AddWithValue("@celular",txtCelular.Text);
+
+            cmd.ExecuteNonQuery();
+            con.FecharConexao();
         }
     }
 }
